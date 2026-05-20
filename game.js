63e1b021a72d420ce58ebae1c97db9ac1620c720
katm6211 <kmalenovic07@gmail.com>
2026-05-20 13:00:25 -0700

    instructions

--- game.js
+++ game.js
@@ -249,7 +249,7 @@ class Intro extends Phaser.Scene {
     create() {
 
         this.add.text(50, 50, "").setFontSize(50);
-        this.add.text(50, 100, "Click anywhere to begin.").setFontSize(20);
+        this.add.text(50, 100, "Click anywhere to begin. Left-click and hold where you want your character to walk to.").setFontSize(20);
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0, 0, 0);
             this.time.delayedCall(1000, () => this.scene.start('demo1'));
